1f74dd916db9c4bc81253462bb66b5933c7e71c5
SuongMai <suongmai2892005@gmail.com>
2025-07-13 16:50:40 +0700

    updated files

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import User from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import AppError from '../utils/appError.js';
-
+import crypto from 'crypto';
 
 const signToken = (id) => {
   if (!process.env.JWT_SECRET) {
@@ -16,7 +16,6 @@ export const register = async (userData) => {
   try {
     console.log('[auth.service.js] Register userData:', userData);
 
-
     const newUser = await User.create(userData);
     console.log('[auth.service.js] New user created:', newUser);
 
@@ -25,7 +24,6 @@ export const register = async (userData) => {
   } catch (error) {
     console.error('[auth.service.js] Register error:', error);
 
-    // ✅ Bắt lỗi trùng username (duplicate key error)
     if (error.code === 11000 && error.keyPattern?.username) {
       throw new AppError('Tên người dùng đã tồn tại!', 400);
     }
@@ -61,9 +59,52 @@ export const login = async (username, password) => {
   }
 };
 
+// Forgot Password
+export const forgotPassword = async (email) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    // Không tiết lộ email tồn tại hay không
+    return;
+  }
+
+  // Tạo token reset password
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  const resetTokenHash = crypto.createHash('sha256').update(resetToken).digest('hex');
+  user.passwordResetToken = resetTokenHash;
+  user.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 phút
+
+  await user.save({ validateBeforeSave: false });
+
+  // TODO: Gửi email chứa link reset (ở đây trả về token để test)
+  return resetToken;
+};
+
+// Reset Password
+export const resetPassword = async (token, newPassword) => {
+  const resetTokenHash = crypto.createHash('sha256').update(token).digest('hex');
+  const user = await User.findOne({
+    passwordResetToken: resetTokenHash,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+
+  if (!user) {
+    throw new AppError('Token không hợp lệ hoặc đã hết hạn', 400);
+  }
+
+  user.password = newPassword;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  const jwtToken = signToken(user._id);
+  return { user, token: jwtToken };
+};
+
 const authService = {
   register,
   login,
+  forgotPassword,
+  resetPassword
 };
 
 export default authService;
